a5f4b1fe08eb298092f90c7bd3f3be0999918425
MJ Soko <lilmariemarie@hotmail.com>
2015-10-24 16:25:05 +0900

    added user wall

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.8/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
+LOGIN_REDIRECT_URL = '/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
